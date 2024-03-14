5ce8ca46cc09c95e931477c6e98a486058bf55f1
edvin <edvin.bergea@gmail.com>
2024-03-14 14:19:30 +0100

    fixed stuff

--- node.c
+++ node.c
@@ -13,14 +13,7 @@ void rtinit(struct distance_table *table, int node)
 	struct rtpkt routing_packet;
 	int min_costs[NCOUNT];
 	for(int i = 0; i<NCOUNT; i++){
-		int min = INF;
-		for(int j = 0; j<NCOUNT; j++){
-			//printf("%d, %d %d\n", i, j, table->costs[j][i]);
-			if(table->costs[j][i] < min){
-				min = table->costs[j][i];
-			}
-		}
-		min_costs[i] = min;
+		min_costs[i] = table->costs[node][i];
 	}
 	//printarr(min_costs, NCOUNT);
 	routing_packet.sourceid = node;
@@ -37,9 +30,12 @@ void rtinit(struct distance_table *table, int node)
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
 	int changed = 0;
 	for(int i = 0; i<NCOUNT; i++){
-		if(pkt->mincost[i] + table->costs[node][pkt->sourceid] < table->costs[pkt->sourceid][i]){
-			table->costs[pkt->sourceid][i] = pkt->mincost[i] + table->costs[node][pkt->sourceid];
+		if(pkt->mincost[i] != table->costs[pkt->sourceid][i]){
+			table->costs[pkt->sourceid][i] = pkt->mincost[i];
 			changed = 1;
+		}
+		if(pkt->mincost[i] + table->costs[node][pkt->sourceid] < table->costs[node][i]){
+			table->costs[node][i] = pkt->mincost[i] + table->costs[node][pkt->sourceid];
 		}
 	}
 	if(changed)
