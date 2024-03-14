29edd64c777c21220d0bb108690855b3a45aad7e
ebl0b <edvin.bergea@gmail.com>
2024-03-14 20:48:35 +0100

    fixed performance

--- node.c
+++ node.c
@@ -1,21 +1,7 @@
 #include "node.h"
 
-static void printarr(int* arr, int size){
-	printf("{");
-	for(int i = 0; i<size; i++){
-		i+1!=size ? printf("%d, ", arr[i]) : printf("%d}\n", arr[i]);
-	}
-}
-
-void rtinit(struct distance_table *table, int node)
-{
-	// Tables are already initialized
+static void rtsend(int* min_costs, int node){
 	struct rtpkt routing_packet;
-	int min_costs[NCOUNT];
-	for(int i = 0; i<NCOUNT; i++){
-		min_costs[i] = table->costs[node][i];
-	}
-	//printarr(min_costs, NCOUNT);
 	routing_packet.sourceid = node;
 	memcpy(routing_packet.mincost, min_costs, NCOUNT*sizeof(int));
 	for(int i = 0; i<NCOUNT; i++){
@@ -24,23 +10,38 @@ void rtinit(struct distance_table *table, int node)
 			tolayer2(routing_packet);
 		}
 	}
-	printdt(table, node);
+}
+
+static void extract_mincosts(int* min_costs, struct distance_table* table, int node){
+	for(int i = 0; i<NCOUNT; i++){
+		min_costs[i] = table->costs[node][i];
+	}
+}
+
+void rtinit(struct distance_table *table, int node)
+{
+	// Tables are already initialized
+	int min_costs[NCOUNT];
+	extract_mincosts(min_costs, table, node);
+	rtsend(min_costs, node);
 }
 
 void rtupdate(struct distance_table *table, int node, struct rtpkt *pkt) {
 	int changed = 0;
+	int stodcost = table->costs[node][pkt->sourceid];
 	for(int i = 0; i<NCOUNT; i++){
-		if(pkt->mincost[i] != table->costs[pkt->sourceid][i]){
+		if((pkt->mincost[i] != table->costs[pkt->sourceid][i]))
 			table->costs[pkt->sourceid][i] = pkt->mincost[i];
+		if(pkt->mincost[i] + stodcost < table->costs[node][i]){
+			table->costs[node][i] = pkt->mincost[i] + stodcost;
 			changed = 1;
 		}
-		if(pkt->mincost[i] + table->costs[node][pkt->sourceid] < table->costs[node][i]){
-			table->costs[node][i] = pkt->mincost[i] + table->costs[node][pkt->sourceid];
-		}
 	}
-	if(changed)
-		rtinit(table, node);
-	printdt(table, node);
+	if(changed){
+		int min_costs[NCOUNT];
+		extract_mincosts(min_costs, table, node);
+		rtsend(min_costs, node);
+	}else printdt(table, node);
 }
 
 
